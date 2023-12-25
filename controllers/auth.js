c8d0aee5b28618f11417d781705c93765c32cf51
nisu.ahmad <nisu.ahmad@samarth.ac.in>
2023-12-25 19:21:21 +0530

    some changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,58 +1,57 @@
-// these allows us to encrypt the password
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js"
+import User from "../models/User.js";
 
-export const register=async(req,res)=>{
+/* REGISTER USER */
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
 
-    try{
-        const {
-            firstName,
-            lastName,
-            email,
-            password,
-            picturePath,
-            friends,
-            location,
-            occupation
-        }=req.body;
-        const salt=await bcrypt.genSalt();
-        const passwordHash=await bcrypt.hash(password,salt);
-        const newUser= new User ({
-            firstName,
-            lastName,
-            email,
-            password:passwordHash,
-            picturePath,
-            friends,
-            location,
-            occupation,
-            viewdPrpfile:Math.floor(Math.random()*10000),
-            impression:Math.floor(Math.random()*10000),
-        })
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
 
-        const savedUser= await newUser.save();
-        res.status(201).json(savedUser);
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
-    }
-    catch(err){
-        res.status(501).json({err:err.message});
-    }
-}
-/* Lgging in */
+/* LOGGING IN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
-export const login= async (req,res)=>{
-    try{
-        const {email,password}=req.body;
-        const user= User.findOne({email:email});
-        if(!user) res.status(400).json({msg:"User Does not Exist"});
-        const isMatch =await bcrypt.compare(password,user.password);
-        if(!isMatch) res.status(400).json({msg:"Invalid Credential"});
-        const token= jwt.sign({id:user._id},process.env.JWT_SECRET);
-        delete user.password;
-        res.status(200).json(token,user)
-    }
-    catch(err){
-        res.status(501).json({err:err.message});
-    }
-}
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,14 @@
 import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
-import {getFeedPost,getUserPost,likePost} from '../controllers/posts.js';
 
-const router=express.Router();
+const router = express.Router();
 
 /* READ */
-
-router.get('/',verifyToken,getFeedPost);
-router.get('/:userId/post',verifyToken,getFeedPost);
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
 
 /* UPDATE */
-router.patch("/:id/like",verifyToken,likePost);
+router.patch("/:id/like", verifyToken, likePost);
+
 export default router;
